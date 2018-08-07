5d69c49d39448aac4c389cb107c567e6d2cba4a2
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-08-07 14:51:35 +0300

    Added serilog

--- src/Nucleus.Web.Api/Program.cs
+++ src/Nucleus.Web.Api/Program.cs
@@ -7,18 +7,56 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Nucleus.Web.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    "Logs/Logs.txt",
+                    fileSizeLimitBytes: 1_000_000,
+                    rollOnFileSizeLimit: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                var host = BuildWebHost(args);
+                //using (var scope = host.Services.CreateScope())
+                //{
+                //    var permissionAppService = scope.ServiceProvider.GetRequiredService<IPermissionAppService>();
+                //    permissionAppService.InitializePermissions(PermissionConsts.AllPermissions());
+                //}
+
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
     }
 }
