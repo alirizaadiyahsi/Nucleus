808e6cf7321ab542e83642fa18e0358d19b1aef2
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-10-06 22:47:10 +0300

    Convert login/register page to responsive Resolved #18

--- src/Nucleus.Web.Vue/src/account/views/login/login.vue
+++ src/Nucleus.Web.Vue/src/account/views/login/login.vue
@@ -1,23 +1,27 @@
 ﻿<template>
-    <div class="login-form">
-        <form v-on:submit.prevent="onSubmit">
-            <h2 class="text-center">Login</h2>
-            <div v-for="error in errors">
-                <div class="alert alert-danger" role="alert">
-                    {{error.value}}
-                </div>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="login-form">
+                <form v-on:submit.prevent="onSubmit">
+                    <h2 class="text-center">Login</h2>
+                    <div v-for="error in errors">
+                        <div class="alert alert-danger" role="alert">
+                            {{error.value}}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" v-model="usernameoremail" class="form-control" placeholder="User name or e-mail" required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" v-model="password" class="form-control" placeholder="Password" required="required">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+                </form>
+                <p class="text-center"><router-link to="/account/register">Register</router-link></p>
             </div>
-            <div class="form-group">
-                <input type="text" v-model="usernameoremail" class="form-control" placeholder="User name or e-mail" required="required">
-            </div>
-            <div class="form-group">
-                <input type="password" v-model="password" class="form-control" placeholder="Password" required="required">
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Login</button>
-            </div>
-        </form>
-        <p class="text-center"><router-link to="/account/register">Register</router-link></p>
+        </div>
     </div>
 </template>
 
--- src/Nucleus.Web.Vue/src/account/views/register/register.vue
+++ src/Nucleus.Web.Vue/src/account/views/register/register.vue
@@ -1,35 +1,39 @@
 ﻿<template>
-    <div v-if="!registerComplete" class="register-form">
-        <form v-on:submit.prevent="onSubmit">
-            <h2 class="text-center">Register</h2>
-            <div v-for="error in errors">
-                <div class="alert alert-danger" role="alert">
-                    {{error.value}}
-                </div>
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div v-if="!registerComplete" class="register-form">
+                <form v-on:submit.prevent="onSubmit">
+                    <h2 class="text-center">Register</h2>
+                    <div v-for="error in errors">
+                        <div class="alert alert-danger" role="alert">
+                            {{error.value}}
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" v-model="username" class="form-control" placeholder="User name" required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" v-model="email" class="form-control" placeholder="E-Mail" required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" v-model="password" class="form-control" placeholder="Password" required="required">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                </form>
+                <p class="text-center"><router-link to="/account/login">Login</router-link></p>
             </div>
-            <div class="form-group">
-                <input type="text" v-model="username" class="form-control" placeholder="User name" required="required">
-            </div>
-            <div class="form-group">
-                <input type="email" v-model="email" class="form-control" placeholder="E-Mail" required="required">
-            </div>
-            <div class="form-group">
-                <input type="password" v-model="password" class="form-control" placeholder="Password" required="required">
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Register</button>
-            </div>
-        </form>
-        <p class="text-center"><router-link to="/account/login">Login</router-link></p>
-    </div>
 
-    <div v-else class="register-form">
-        <form v-on:submit.prevent="onSubmit">
-            <h2 class="text-center">Register</h2>
-            <div class="alert alert-success" role="alert">
-                {{resultMessage}} Please <router-link to="/account/login">Login</router-link>.
+            <div v-else class="register-form">
+                <form v-on:submit.prevent="onSubmit">
+                    <h2 class="text-center">Register</h2>
+                    <div class="alert alert-success" role="alert">
+                        {{resultMessage}} Please <router-link to="/account/login">Login</router-link>.
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </template>
 
