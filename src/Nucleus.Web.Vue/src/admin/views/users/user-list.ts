4b6abe610b03e091330cfab0c09c4ce882828b84
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-10-23 23:07:45 +0300

    #22 - Added remove action to userList page

--- src/Nucleus.Web.Vue/src/admin/views/users/user-list.ts
+++ src/Nucleus.Web.Vue/src/admin/views/users/user-list.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import UserAppService from '../../../services/user-app-service';
+import swal from 'sweetalert2';
 
 @Component
 export default class UserListComponent extends Vue {
@@ -11,12 +12,48 @@ export default class UserListComponent extends Vue {
     public userAppService = new UserAppService();
 
     public mounted() {
+        this.getUsers();
+    }
+
+    public getUsers() {
         const userListInput: IUserListInput = {
             filter: '',
         };
 
         this.userAppService.getUsers(userListInput).then((response) => {
             this.pagedListOfUserListDto = response.content as IPagedList<IUserListInput>;
+        });
+    }
+
+    public remove(id: string) {
+        swal({
+            title: 'Are you sure want to delete?',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.value) {
+                this.userAppService.deleteUser(id).then((response) => {
+                    if (!response.isError) {
+                        swal({
+                            toast: true,
+                            position: 'bottom-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                            type: 'success',
+                            title: 'Successfully deleted!'
+                        });
+                        this.getUsers();
+                    } else {
+                        swal({
+                            title: response.errors.join('<br>'),
+                            type: 'error',
+                            showConfirmButton: false,
+                        });
+                    }
+                });
+            }
         });
     }
 }
--- src/Nucleus.Web.Vue/src/admin/views/users/user-list.vue
+++ src/Nucleus.Web.Vue/src/admin/views/users/user-list.vue
@@ -5,12 +5,22 @@
         <table v-if="pagedListOfUserListDto.totalCount" class="table">
             <thead>
                 <tr>
+                    <th>Actions</th>
                     <th>User Name</th>
                     <th>E-Mail</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="item in pagedListOfUserListDto.items" :key="item.id">
+                    <td>
+                        <div class="btn-group">
+                            <button v-if="item.userName != 'admin'"
+                                    v-on:click="remove(item.id)"
+                                    class="btn btn-danger btn-sm">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </td>
                     <td>{{ item.userName }}</td>
                     <td>{{ item.email }}</td>
                 </tr>
