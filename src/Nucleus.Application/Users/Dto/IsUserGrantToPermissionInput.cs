cade9076e191de8efc786cb33b5a1ce134efcf95
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-11-20 21:06:30 +0300

    Seperate models related account in Application/Users/Dto fixed #88

--- src/Nucleus.Application/Users/Dto/IsUserGrantToPermissionInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nucleus.Application.Users.Dto
-{
-    public class IsUserGrantToPermissionInput
-    {
-        public string UserNameOrEmail { get; set; }
-
-        public string PermissionName { get; set; }
-    }
-}
