e3800fa4b09d46c19d584fe2aee8db463aa42700
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2018-12-04 21:27:27 +0300

    Add list to home page with dummy data. Fixes: #120

--- src/Nucleus.Web.Vue/src/admin/views/home/home.vue
+++ src/Nucleus.Web.Vue/src/admin/views/home/home.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-    <v-layout row>
+    <v-layout row wrap>
         <v-flex md6>
             <v-card>
                 <v-card-title>
@@ -34,6 +34,50 @@
                              auto-draw></v-sparkline>
             </v-card>
         </v-flex>
+        <v-flex md4>
+            <v-card>
+                <v-toolbar color="cyan" dark>
+                    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+                    <v-toolbar-title>Inbox</v-toolbar-title>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn icon>
+                        <v-icon>search</v-icon>
+                    </v-btn>
+                </v-toolbar>
+
+                <v-list two-line>
+                    <template v-for="(item, index) in items">
+                        <v-subheader v-if="item.header"
+                                     :key="item.header">
+                            {{ item.header }}
+                        </v-subheader>
+
+                        <v-divider v-else-if="item.divider"
+                                   :inset="item.inset"
+                                   :key="index"></v-divider>
+
+                        <v-list-tile v-else
+                                     :key="item.title"
+                                     avatar
+                                     @click="">
+                            <v-list-tile-avatar>
+                                <img :src="item.avatar">
+                            </v-list-tile-avatar>
+
+                            <v-list-tile-content>
+                                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </template>
+                </v-list>
+            </v-card>
+        </v-flex>
+        <v-flex md4></v-flex>
+        <v-flex md4></v-flex>
     </v-layout>
 </template>
 
