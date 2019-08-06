8bf29065ede54baaac53e21f9fff6ad160cb1dcb
alirizaadiyahsi <aliriza.adiyahsi@volosoft.com>
2019-08-06 15:00:59 +0300

    Fixed lint warnings

--- src/nucleus-web-vue/src/plugins/vuetify.ts
+++ src/nucleus-web-vue/src/plugins/vuetify.ts
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
-  },
+    icons: {
+        iconfont: "mdi"
+    }
 });
