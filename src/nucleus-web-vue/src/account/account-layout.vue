a13ea45efd210d6e7e7d948ca0e0f2fe8ac58505
alirizaadiyahsi <aliriza.adiyahsi@volosoft.com>
2019-08-06 23:53:02 +0300

    Upgraded vuetify menu compnent

--- src/nucleus-web-vue/src/account/account-layout.vue
+++ src/nucleus-web-vue/src/account/account-layout.vue
@@ -4,23 +4,25 @@
             <v-layout align-center justify-center>
                 <v-flex xs12 sm8 md4>
                     <router-view></router-view>
-                    <v-layout align-center justify-center row fill-height>
-                        <v-menu class="mt-3">
-                            <v-btn v-slot:activator="{ on }" color="primary" dark outlined rounded>
-                                <img :src="require('@/assets/images/icons/flags/' + selectedLanguage.languageCode + '.png')" class="mr-2 ml-1" />
-                                {{selectedLanguage.languageName}}
-                                <v-icon dark class="ml-3">arrow_drop_down</v-icon>
-                            </v-btn>
+                    <v-layout class="mt-3" align-center justify-center>
+                        <v-menu>
+                            <template v-slot:activator="{ on }">
+                                <v-btn color="primary" dark outlined rounded v-on="on">
+                                    <img :src="require('@/assets/images/icons/flags/' + selectedLanguage.languageCode + '.png')" class="mr-2 ml-1" />
+                                    {{selectedLanguage.languageName}}
+                                    <v-icon dark class="ml-3">arrow_drop_down</v-icon>
+                                </v-btn>
+                            </template>
 
                             <v-list>
-                                <v-list-tile @click="changeLanguage('en', 'English')">
+                                <v-list-item @click="changeLanguage('en', 'English')">
                                     <img src="@/assets/images/icons/flags/en.png" class="mr-2" />
-                                    <v-list-tile-title>English</v-list-tile-title>
-                                </v-list-tile>
-                                <v-list-tile @click="changeLanguage('tr', 'Türkçe')">
+                                    <v-list-item-title>English</v-list-item-title>
+                                </v-list-item>
+                                <v-list-item @click="changeLanguage('tr', 'Türkçe')">
                                     <img src="@/assets/images/icons/flags/tr.png" class="mr-2" />
-                                    <v-list-tile-title>Türkçe</v-list-tile-title>
-                                </v-list-tile>
+                                    <v-list-item-title>Türkçe</v-list-item-title>
+                                </v-list-item>
                             </v-list>
                         </v-menu>
                     </v-layout>
