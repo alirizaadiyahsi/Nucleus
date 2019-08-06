44b731cadcbe782c8a9514fcd019b68d738cd0c4
alirizaadiyahsi <aliriza.adiyahsi@volosoft.com>
2019-08-06 15:05:16 +0300

    linter options

--- src/nucleus-web-vue/.eslintrc.js
+++ src/nucleus-web-vue/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "indent": ["error", "tab"]
+        "indent": ["error", 4]
     },
     parserOptions: {
         parser: "@typescript-eslint/parser"
--- src/nucleus-web-vue/src/router.ts
+++ src/nucleus-web-vue/src/router.ts
@@ -5,22 +5,22 @@ import Home from "./views/Home.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
+        {
+            path: "/about",
+            name: "about",
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () =>
+                import(/* webpackChunkName: "about" */ "./views/About.vue")
+        }
+    ]
 });
