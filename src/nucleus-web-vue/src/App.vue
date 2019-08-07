49fed56108a55f9b78d854de4eec09c085d3e55b
alirizaadiyahsi <aliriza.adiyahsi@volosoft.com>
2019-08-07 16:08:56 +0300

    Changed loading color

--- src/nucleus-web-vue/src/App.vue
+++ src/nucleus-web-vue/src/App.vue
@@ -5,7 +5,7 @@
             <v-dialog v-model="nucleus.isLoading" persistent content content-class="centered-dialog">
                 <v-container fill-height>
                     <v-layout column justify-center align-center>
-                        <v-progress-circular indeterminate :size="70" :width="7" :color="'purple'"></v-progress-circular>
+                        <v-progress-circular indeterminate :size="70" :width="7" :color="'primary'"></v-progress-circular>
                         <h1>Loading...</h1>
                     </v-layout>
                 </v-container>
