db956a45855f8182575113e682d6dca2899c620a
alirizaadiyahsi <aliriza.adiyahsi@volosoft.com>
2019-08-07 05:56:45 +0300

    Fixed linlint errors

--- src/nucleus-web-vue/.eslintrc.js
+++ src/nucleus-web-vue/.eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
         "indent": ["error", 4],
-        "quotes": [2, "single", { "avoidEscape": true }]
+        "quotes": [2, "single", { "avoidEscape": true }],
+        "eol-last": ["error", "never"]
     },
     parserOptions: {
         parser: "@typescript-eslint/parser"
