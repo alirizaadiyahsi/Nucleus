13ed2df20c3b466913f28ce6d4a76338a281fd66
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-12-31 23:33:38 +0300

    Update RoleAppServiceTests.cs

--- test/Nucleus.Tests.Application/Roles/RoleAppServiceTests.cs
+++ test/Nucleus.Tests.Application/Roles/RoleAppServiceTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Nucleus.Application.Roles;
 using Nucleus.Application.Roles.Dto;
